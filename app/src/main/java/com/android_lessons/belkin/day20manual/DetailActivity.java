8c4fbc0e093f3210c10d3ece0e143b354f8bc3ea
Belka <Evgenia_@email.ua>
2015-04-16 15:19:52 +0300

    add functionality

--- app/src/main/java/com/android_lessons/belkin/day20manual/DetailActivity.java
+++ app/src/main/java/com/android_lessons/belkin/day20manual/DetailActivity.java
@@ -1,9 +1,17 @@
 package com.android_lessons.belkin.day20manual;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.webkit.WebView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 
 public class DetailActivity extends ActionBarActivity {
@@ -12,6 +20,38 @@ public class DetailActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        WebView webView  = (WebView) findViewById(R.id.webView);
+
+        Intent intent = getIntent();
+        String resName = "n" + intent.getIntExtra("head", 0); //получаем строку и формируем имя ресурса
+
+        Context context = getBaseContext(); //получаем контекст
+
+        //читаем текстовый файл из ресурсов по имени
+        String text = readRawTextFile(context, getResources().getIdentifier(resName, "raw", "com.android_lessons.belkin.day20manual"));
+
+        webView.loadDataWithBaseURL(null, text, "text/html", "en_Us", null);
+    }
+
+    //читаем текст из raw-ресурсов
+    public static String readRawTextFile(Context context, int resId){
+        InputStream inputStream = context.getResources().openRawResource(resId);
+
+        InputStreamReader inputReader = new InputStreamReader(inputStream);
+        BufferedReader buffReader = new BufferedReader(inputReader);
+        String line;
+        StringBuilder builder = new StringBuilder();
+
+        try{
+            while((line = buffReader.readLine()) != null){
+                builder.append(line);
+                builder.append("\n");
+            }
+        }catch(IOException e){
+            return null;
+        }
+        return builder.toString();
     }
 
 
--- app/src/main/java/com/android_lessons/belkin/day20manual/MainActivity.java
+++ app/src/main/java/com/android_lessons/belkin/day20manual/MainActivity.java
@@ -1,19 +1,53 @@
 package com.android_lessons.belkin.day20manual;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    //Создаем массив разделов:
+    private String head_array[] = {
+            "00. Начало",
+            "01. Чем кормить кота.",
+            "02. Как гладить кота.",
+            "03. Как спит кот.",
+            "04. Как играть с котом.",
+            "05. Как разговаривать с котом",
+            "06. Интересные факты из жизни котов.",
+            "07. Как назвать кота.",
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+        // Получим идентификатор ListView
+        ListView listView = (ListView) findViewById(R.id.listView);
+        //устанавливаем массив в ListView
+        listView.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, head_array));
+        listView.setTextFilterEnabled(true);
+
+        //Обрабатываем щелчки на элементах ListView:
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> a, View v, int position, long id) {
+                Intent intent = new Intent();
+                intent.setClass(MainActivity.this, DetailActivity.class);
+                intent.putExtra("head", position);
+
+                //запускаем вторую активность
+                startActivity(intent);
+            }
+        });
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
